d05aade8a48389e74d8cea745e89347f3554d4b1
naimur rahman rumel <88675051+naimur1046@users.noreply.github.com>
2023-01-07 11:52:10 +0600

    Update Extended_Euclid.cpp

--- algorithm/Extended_Euclid.cpp
+++ algorithm/Extended_Euclid.cpp
@@ -1,6 +1,17 @@
 #include<bits\stdc++.h>
+
 using namespace std;
+pair<int,int> extended_gcd(int a,int b)
+{
+    if(b==0) return {1,1};
+    auto [x2,y2]=extended_gcd(b,a%b);
+    int x=y2,y=x2-((a/b)*y2);
+    return{x,y};
+}
 int main()
 {
-  
+    int a,b;
+    cin >> a >> b;
+    auto [x,y]= extended_gcd(a,b);
+    cout << x << " " << y << endl;
 }
