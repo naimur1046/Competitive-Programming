4b4ab7ac99cf4625b153137ceb9c4fa2ff2b2885
Naimur Rahman Rumel <88675051+naimur1046@users.noreply.github.com>
2024-05-10 08:34:38 +0600

    Update BubbleSort.cpp

--- Sorting Technique/BubbleSort.cpp
+++ Sorting Technique/BubbleSort.cpp
@@ -1,1 +1,26 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int n;
+     cin >> n;
+     vector<int> numbers(n);
+     for (int i = 0; i < n; i++)
+     {
+          cin >> numbers[i];
+     }
+     int count = 1;
+     while (count < n)
+     {
+          for (int i = 0; i < n - count; i++)
+          {
+               if (numbers[i] > numbers[i + 1])
+               {
+                    swap(numbers[i], numbers[i + 1]);
+               }
+          }
+          count++;
+     }
+     return 0;
+}
