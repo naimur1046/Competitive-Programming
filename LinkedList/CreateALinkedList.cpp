ec5b78200101cfe98405b5f03457cce3f90b51d1
Naimur Rahman Rumel <88675051+naimur1046@users.noreply.github.com>
2024-05-12 10:33:06 +0600

    Update CreateALinkedList.cpp

--- LinkedList/CreateALinkedList.cpp
+++ LinkedList/CreateALinkedList.cpp
@@ -1,1 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node
+{
+public:
+     int data;
+     node *next;
+     node(int val)
+     {
+          data = val;
+          next = NULL;
+     }
+};
 
+void insetAtTail(node *&n, int val)
+{
+     node *temp = n;
+     if (temp == NULL)
+          n = new node(val);
+     while (temp->next != NULL)
+     {
+          temp = temp->next;
+     }
+     temp->next = new node(val);
+}
+
+void display(node *&n)
+{
+     node *temp = n;
+     while (temp != NULL)
+     {
+          cout << temp->data << endl;
+          temp = temp->next;
+     }
+}
+int main()
+{
+     node *nod = new node(5);
+     insetAtTail(nod, 15);
+     insetAtTail(nod, 1);
+     insetAtTail(nod, 10);
+     display(nod);
+}
