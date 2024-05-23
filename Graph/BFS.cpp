f86fc7885f30cc1b9a30291ebc409ac8e80f52b0
Naimur Rahman Rumel <88675051+naimur1046@users.noreply.github.com>
2024-05-23 08:39:19 +0600

    Create BFS.cpp

---
+++ Graph/BFS.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e3;
+vector<bool> visited(N);
+vector<int> graph[N];
+void bfs(int node)
+{
+     queue<int> q;
+     q.push(node);
+     visited[node] = true;
+     while (!q.empty())
+     {
+          int cur_vertex = q.front();
+          q.pop();
+          for (auto x : graph[cur_vertex])
+          {
+               if (!visited[x])
+               {
+                    q.push(x);
+                    visited[x] = true;
+               }
+          }
+     }
+}
+int main()
+{
+     int n, e;
+     cin >> n >> e;
+     for (int i = 0; i < e; i++)
+     {
+          int v1, v2;
+          cin >> v1 >> v2;
+          graph[v1].push_back(v2);
+          graph[v2].push_back(v1);
+     }
+     bfs(1);
+}
