7a6861952a38b0211fb946bf178c84fe6350c5c0
Naimur Rahman Rumel <88675051+naimur1046@users.noreply.github.com>
2024-06-05 09:09:59 +0600

    Update LongestCommonSubsequence.cpp

--- DP/LongestCommonSubsequence.cpp
+++ DP/LongestCommonSubsequence.cpp
@@ -2,17 +2,22 @@
 using namespace std;
 const int N = 1e5 + 10;
 vector<int> nums(N);
+int dp[N];
 int func(int i)
 {
+     if (dp[i] != -1)
+          return dp[i];
      int res = 1;
      for (int j = i - 1; j >= 0; j--)
      {
           if (nums[j] < nums[i])
                res = max(func(j) + 1, res);
      }
+     return res;
 }
 int main()
 {
+     memset(dp, -1, N);
      int n;
      cin >> n;
      for (int i = 0; i < n; i++)
